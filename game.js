c8234279371ab447fb5796190e0a531b8cfeb837
Osman Talha Umucu <osmanumucu40@gmail.com>
2024-05-22 13:59:56 +0200

    165 completed

--- game.js
+++ game.js
@@ -1,5 +1,13 @@
+var buttoncolours = ["red", "blue", "green", "yellow"];
+var gamepattern = [];
+
 function nextSequence() {
     
     var randomnumber = Math.floor(Math.random() * 4);
 
+
+    var randomChosenColour = buttoncolours[randomnumber]
+
+    gamepattern.push(randomChosenColour);
 }
+
