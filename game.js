456cb657097413ff85b27eac7c26078cce796fc7
Osman Talha Umucu <osmanumucu40@gmail.com>
2024-05-22 14:26:36 +0200

    167

--- game.js
+++ game.js
@@ -4,10 +4,18 @@ var gamepattern = [];
 function nextSequence() {
     
     var randomnumber = Math.floor(Math.random() * 4);
-
-
+    
     var randomChosenColour = buttoncolours[randomnumber]
 
     gamepattern.push(randomChosenColour);
-}
 
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+
+    function playSound(name) {
+
+        var audio = new Audio("sounds/" + name + ".mp3");
+        audio.play();
+}}
+
+nextSequence();
